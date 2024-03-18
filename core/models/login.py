ffa343f451eb2592895b364e4a997b723c721a4b
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-18 15:45:02 -0300

    created: API e rotas para o login

---
+++ core/models/login.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Login(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    def __str__(self):
+        return self.nome
---
+++ core/serializers/login.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from core.models import Login
+
+class LoginSerializer(ModelSerializer):
+    class Meta:
+        model = Login
+        fields = "__all__"
